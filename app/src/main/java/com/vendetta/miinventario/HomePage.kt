00aec2aecf3e05879faf4baa2eaa4212c392919f
Marvin Zavala <hazielzs@hotmail.com>
2024-01-09 18:41:43 -0600

    Add graph and date ranger picker

---
+++ app/src/main/java/com/vendetta/miinventario/HomePage.kt
@@ -0,0 +1,137 @@
+package com.vendetta.miinventario
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.util.Pair
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
+import com.vendetta.miinventario.databinding.ActivityHomePageBinding
+import java.security.KeyStore.Entry
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class HomePage : AppCompatActivity() {
+    lateinit var binding : ActivityHomePageBinding
+    val DAY_IN_MILISECONDS = 86400000
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Cuando el boton de seleccionar es presionado desplegar date ranger picker
+        binding.btnSelectDate.setOnClickListener {
+            //Mostrar Date Picker
+            showDateRangePicker()
+            }
+
+        configLinearChart()
+
+    }
+
+    /************************************************************
+     * ConfigLinearChart
+     * Esta es la funcion para configurar el grafico de lineas
+     **************************************************************/
+    private fun configLinearChart() {
+        //GET THE FONT
+        val typeface = ResourcesCompat.getFont(this,R.font.mairyextrabold)
+        //Obtener el linechart id desde el layout
+        val lineChart = binding.linearChart
+
+        //Aqui van las entradas al grafico
+        val entries = ArrayList<com.github.mikephil.charting.data.Entry>()
+        entries.add(com.github.mikephil.charting.data.Entry(1f,120.00f))
+        entries.add(com.github.mikephil.charting.data.Entry(2f,180.00f))
+        entries.add(com.github.mikephil.charting.data.Entry(3f,240.00f))
+        entries.add(com.github.mikephil.charting.data.Entry(4f,450.00f))
+        entries.add(com.github.mikephil.charting.data.Entry(5f,200.00f))
+        entries.add(com.github.mikephil.charting.data.Entry(6f,350.00f))
+        entries.add(com.github.mikephil.charting.data.Entry(7f,450.00f))
+
+        //Aqui comienza la configuracion
+        val dataSet = LineDataSet(entries, "Ventas") // Agrega entradas a tu conjunto de datos
+       //Configuracion para la linea
+        dataSet.color = Color.GREEN //Color de la linea
+        dataSet.valueTextColor = Color.WHITE //Color del texto de la linea
+        dataSet.setDrawValues(true) //Para la animacion
+        dataSet.valueTextSize = 10f // El tamaño de las letras de la liena
+        dataSet.valueTypeface = typeface // La fuente de las letras de la liena
+        dataSet.setCircleColor(Color.WHITE) //El color del borde el circulo de la liena
+        dataSet.circleHoleColor = Color.GREEN //El color del circulo de la linea
+        val lineData = LineData(dataSet) //Guardar toda esa configuracion para desplegar el chart
+        lineChart.data = lineData //Introducir la configuracion en el chart
+
+        //Esta es la configuracion para la descripcion del grafico que se encuentra esquina inferior derecha
+        lineChart.description = Description().apply {
+            this.text = "Ultimos 7 dias"; //Texto
+            this.textColor = Color.WHITE; //Color
+            this.typeface = typeface; //Fuente
+            this.textSize = 20f; //Tamaño de la letra
+            this.yOffset = -20f //Posicion
+        }
+        //Esta es la configuracion para el texto que se encuentra en la esquina inferior derecha que contiene el color de la linea
+        lineChart.legend.textColor = Color.WHITE //Color del texto
+        lineChart.legend.typeface = typeface //Fuente
+        lineChart.legend.textSize = 15f //Tamaño de la letra
+
+        //Esta es la configuracion para las letras de la parte superior del chart
+        lineChart.xAxis.textColor = Color.WHITE //Color
+        lineChart.xAxis.typeface = typeface //Fuente
+
+        //Esta es la configuracion para las letras del lado izquierdo
+        lineChart.axisLeft.textColor = Color.WHITE //Color
+        lineChart.axisLeft.typeface = typeface //Fuente
+
+        //Esta es la configuracion para las letras del lado derecho
+        lineChart.axisRight.textColor = Color.WHITE
+        lineChart.axisRight.typeface = typeface
+
+        //Esta es la configuracion del chart en la parte externa
+        lineChart.setExtraOffsets(10f, 20f, 10f, 10f) //Padding
+        lineChart.animateX(2000) //Animacion desplegandose el chart
+
+        //Esta es una configuracion para remplazar los nombres de las entradas X position
+        val stringArrays = listOf<String>("","Dia 1","Dia 2","Dia 3","Dia 4","Dia 5","Dia 6","Dia 7")
+        lineChart.xAxis.valueFormatter= IndexAxisValueFormatter(stringArrays)
+
+        lineChart.invalidate() // Refresca el gráfico
+    }
+
+    /********************************************************************************************************
+     * Show Date Ranger Picker
+     * Esta funcion es para desplegar el Date Range Picker y deshabilitarlo mientras se escoje una fecha
+     ******************************************************************************************************/
+    private fun showDateRangePicker(){
+        binding.btnSelectDate.isEnabled = false
+        val picker = MaterialDatePicker.Builder.dateRangePicker()
+            .setTitleText("Selecciona 2 fechas")
+            .build()
+        picker.show(this.supportFragmentManager,"DATE_PICKER")
+
+        picker.addOnPositiveButtonClickListener {
+            val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val firstDate = simpleDateFormat.format(it.first + DAY_IN_MILISECONDS)
+            val secondDate = simpleDateFormat.format(it.second + DAY_IN_MILISECONDS)
+            binding.dateSelectedText.text = "$firstDate - $secondDate"
+            binding.btnSelectDate.isEnabled = true
+        }
+
+        picker.addOnNegativeButtonClickListener {
+            binding.btnSelectDate.isEnabled = true
+        }
+        picker.addOnDismissListener {
+            binding.btnSelectDate.isEnabled = true
+        }
+    }
+}
--- app/src/main/java/com/vendetta/miinventario/MainActivity.kt
+++ app/src/main/java/com/vendetta/miinventario/MainActivity.kt
@@ -1,37 +1,28 @@
 package com.vendetta.miinventario
 
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.tasks.await
-
-private lateinit var auth: FirebaseAuth
-private lateinit var database:HashMap<String,Any?>
-//Variable para saber si esta logeado en Firebase auth
-var isLogin = true
-private var backPressedline =0L;
+import com.vendetta.miinventario.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
+private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //JUST FOR TESTING PURPOSE
+        Intent(this,HomePage::class.java).apply { startActivity(this) }
+
+        binding.btnLogin.setOnClickListener {
+            Intent(this,HomePage::class.java).apply { startActivity(this) }
+            binding.editTextText.text.clear()
+            binding.editPin.text.clear()
+        }
+    }
 
     }
 
